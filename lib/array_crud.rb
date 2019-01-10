eeb00374d3385e23e29f5b84d0c208552d50d0f9
Njenga Kariuki <njengak@me.com>
2019-01-10 16:07:12 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -31,7 +31,7 @@ end
 def retrieve_first_element_from_array(array)
   array[0]
 end
-
-def retrieve_last_element_from_array(array)
   
+def retrieve_last_element_from_array(array)
+  array[array.length]  
 end
